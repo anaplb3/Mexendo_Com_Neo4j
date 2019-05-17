5d60548a708b9d6014e056a8a6fe2cd5973875e5
anaplb3 <ana.paula@dce.ufpb.br>
2019-05-17 17:26:47 -0300

    json consertado

--- banco.py
+++ banco.py
@@ -9,26 +9,26 @@ class Dao:
     def get_licitacoes(self, num_resultados):
         result = self.graph.run('match (x:Licitacao) return x limit {num}', num=num_resultados)
         nodes = [n for n in result]
-        return json.dumps(nodes)
+        return nodes
 
     def get_participantes(self, num_resultados):
         result = self.graph.run('match (x:Participante) return x limit {num}', num=num_resultados)
         nodes = [n for n in result]
-        return json.dumps(nodes)
+        return nodes
 
     def get_licitacao_nomeunidadegestora(self, nome):
         result = self.graph.run('match (lic:Licitacao{NomeUnidadeGest:$nome}) return lic', nome=nome)
         nodes = [n for n in result]
-        return json.dumps(nodes)
+        return nodes
 
     def get_licitacao_por_ano(self, ano):
         result = self.graph.run('match (lic:Licitacao) WHERE lic.Data CONTAINS {ano} return lic', ano=ano)
         nodes = [n for n in result]
-        return json.dumps(nodes)
+        return nodes
 
     # Busca participante pelo cpf ou cnpj
     def get_participante_por_codigo(self, codigo):
         result = self.graph.run("match (part:Participante{ChaveParticipante:$codigo}) return part", codigo=codigo)
         nodes = [n for n in result]
-        return json.dumps(nodes)
+        return nodes
 
