89d798825654b677ce63c7d1b9f7772e1e596d71
anaplb3 <ana.paula@dce.ufpb.br>
2019-05-17 13:52:10 -0300

    tentando consertar o connection refused

--- api.py
+++ api.py
@@ -3,29 +3,23 @@ from py2neo import Graph
 from banco import Dao
 
 app = Flask(__name__)
-#graph = Graph("bolt://localhost:7687", auth=("neo4j", "anaehtop"))
 dao = Dao()
 
-# print(result)
-
-#print(dao.get_licitacao_nomeunidadegestora("Prefeitura Municipal de Santa In\u00eas"))
-
-
-@app.route('/busca')
+@app.route('/')
 def olar():
-    result = dao.get_participante_por_codigo("10578305000139")
-    return jsonify(result)
+    return "inicio"
 
 
-@app.route("/ano")
-def busca_ano():
-   result = dao.get_licitacao_por_ano("2016")
-   return jsonify(result)
-
-@app.route('/lic')
-def get_lic():
+@app.route('/licitacoes')
+def get_json_licitacoes():
    result = dao.get_licitacoes()
    return jsonify(result)
 
+@app.route('/participantes')
+def get_json_participantes():
+   result = dao.get_participantes()
+   return jsonify(result)
 
-app.run(debug=True, host="0.0.0.0")
+#print(dao.get_licitacao_nomeunidadegestora("Câmara Municipal de Alcantil"))
+
+app.run(debug=True)
